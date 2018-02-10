eec35a6e72e70cb2ed9206130ac27c659ab741c0
Adam Tasma <adamtasma@gmail.com>
2018-02-10 13:09:15 -0500

    Add files via upload

---
+++ Bonus Lab17 v2/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusLab_17_v2
+{
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double Price { get; set; }
+
+        public Car()
+        {
+            Make = "Tesla";
+            Model = "Model E";
+            Year = 2019;
+            Price = 1000000;
+
+        }
+        public Car(string make, string model, int year, double price)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.Price = price;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} \t{1} \t{2} \t{3}", this.Make, this.Model, this.Year, this.Price);
+        }
+
+    }
+}
---
+++ Bonus Lab17 v2/CarApp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusLab_17_v2
+{
+    static class CarApp
+    {
+        public static int carCount;
+        public static List<Car> carList = new List<Car>();
+        public static void UserInput()
+        {
+            Console.WriteLine("how many cars would you like to input data for?");
+            carCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < carCount; i++)
+            {
+                Car c = new Car();
+                Console.WriteLine("-----New Car-----");
+                Console.WriteLine("what is the car's make?");
+                c.Make = Console.ReadLine();
+                Console.WriteLine("What is the car's model?");
+                c.Model = Console.ReadLine();
+                Console.WriteLine("What is the car's year?");
+                c.Year = int.Parse(Console.ReadLine());
+                Console.WriteLine("What is the car's price?");
+                c.Price = double.Parse(Console.ReadLine());
+                carList.Add(c);
+                //Console.WriteLine(c.Model);
+            }
+            //Console.WriteLine(carList.Count);
+            PrintCarList();
+        }
+        public static void PrintCarList()
+        {
+            Console.WriteLine("current car list");
+            foreach (Car auto in carList)
+            {
+                //Console.WriteLine(Car.Make);
+                Console.WriteLine(auto);
+            }
+        }
+
+
+    }
+}
